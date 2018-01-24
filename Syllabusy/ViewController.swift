0c8f999499dc60dc6f93aff465d4a0927bce83ad
Vivian Tran <ntran.vivian@gmail.com>
2018-01-24 12:16:49 -0800

    Initialized with Firebase

---
+++ Syllabusy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Syllabusy
+//
+//  Created by Vivian Tran on 1/24/18.
+//  Copyright © 2018 Vivian Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
