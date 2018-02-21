998df29c33e239165afe29e1025494b61b74bdbc
Vivian Tran <ntran.vivian@gmail.com>
2018-02-21 15:25:07 -0800

    Working OCR + VisionML

--- Syllabusy/ViewController.swift
+++ Syllabusy/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController, G8TesseractDelegate {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var origImageView: UIImageView!
     @IBOutlet weak var boxedImageView: UIImageView!
-    let image = UIImage(named: "image_sample.jpg")?.g8_blackAndWhite()
+    let image = UIImage(named: "sampleAssignments.jpg")?.g8_blackAndWhite()
+    var recognizedText:[String] = [String]()
+    var recognizedWords:[String] = [String]()
     
     
     
@@ -31,7 +33,7 @@ class ViewController: UIViewController, G8TesseractDelegate {
         super.viewDidLoad()
         origImageView.image = image
         boxedImageView.image = image
-        guard let uiImage = UIImage(named: "image_sample.jpg")?.g8_blackAndWhite()
+        guard let uiImage = UIImage(named: "sampleAssignments.jpg")?.g8_blackAndWhite()
             else { fatalError("no image from image picker") }
         guard let ciImage = CIImage(image: uiImage)
             else { fatalError("can't create CIImage from UIImage") }
@@ -85,43 +87,64 @@ class ViewController: UIViewController, G8TesseractDelegate {
             return
         }
         DispatchQueue.main.async {
-            self.boxedImageView.subviews.forEach({ (s) in
-                s.removeFromSuperview()
-            })
+            //sentences
             for box in observations {
-                guard let chars = box.characterBoxes else {
-                    print("no char values found")
-                    return
-                }
-                let view = self.CreateBoxView(withColor: UIColor.green)
+//                guard let chars = box.characterBoxes else {
+//                    print("no char values found")
+//                    return
+//                }
+                let view = self.CreateBoxView(withColor: UIColor.red)
+                // Shrinks box to boundingbox
                 view.frame = self.transformRect(fromRect: box.boundingBox, toViewRect: self.boxedImageView)
-                self.boxedImageView.image = self.origImageView.image
                 self.boxedImageView.addSubview(view)
-                
-                for char in chars
-                {
-                    let view = self.CreateBoxView(withColor: UIColor.green)
-                    view.frame = self.transformRect(fromRect: char.boundingBox, toViewRect: self.boxedImageView)
-                    self.boxedImageView.image = self.origImageView.image
-                    self.boxedImageView.addSubview(view)
-                }
+                let croppedImage = self.crop(image: (self.image)!, rectangle: box)
+                self.recognizeText(image: croppedImage!)
+//                for char in chars
+//                {
+//                    let view = self.CreateBoxView(withColor: UIColor.green)
+//                    view.frame = self.transformRect(fromRect: char.boundingBox, toViewRect: self.boxedImageView)
+//                    self.boxedImageView.image = self.origImageView.image
+//                    self.boxedImageView.addSubview(view)
+//
+//                }
+                self.recognizedText.append("\n")
+            }
+            for char in self.recognizedText{
+                print(char)
             }
         }
-        
     }
     
-    func recognizeText() {
-        var tesseract:G8Tesseract = G8Tesseract(language:"eng")
+    func crop(image: UIImage, rectangle: VNTextObservation) -> UIImage? {
+        var t: CGAffineTransform = CGAffineTransform.identity;
+        t = t.scaledBy(x: image.size.width, y: -image.size.height);
+        t = t.translatedBy(x: 0, y: -1 );
+        let x = rectangle.boundingBox.applying(t).origin.x
+        let y = rectangle.boundingBox.applying(t).origin.y
+        let width = rectangle.boundingBox.applying(t).width
+        let height = rectangle.boundingBox.applying(t).height
+        let fromRect = CGRect(x: x, y: y, width: width, height: height)
+        let drawImage = image.cgImage!.cropping(to: fromRect)
+        if let drawImage = drawImage {
+            let uiImage = UIImage(cgImage: drawImage)
+            return uiImage
+        }
+        return nil
+    }
+    
+    func recognizeText(image: UIImage) {
+        let tesseract:G8Tesseract = G8Tesseract(language:"eng")
+        tesseract.charWhitelist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         tesseract.delegate = self
         tesseract.image = image
         tesseract.recognize()
         
-        textView.text = tesseract.recognizedText
-        print(tesseract.recognizedText)
+        textView.text = tesseract.recognizedText.lowercased()
+        let letter = String(tesseract.recognizedText.filter{ !"\n".contains($0) })
+        recognizedText.append(letter)
     }
     
     func shouldCancelImageRecognitionForTesseract(tesseract: G8Tesseract!) -> Bool {
         return false // return true if you need to interrupt tesseract before it finishes
     }
 }
-
